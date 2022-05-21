e6e1158db07541f5d9f008b1db2fe54c54cf0c56
@afate <afate@student.21-school.ru>
2022-05-22 00:33:39 +0700

    20

--- social/src/App.js
+++ social/src/App.js
@@ -3,7 +3,10 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 function App() {
   return (
@@ -11,10 +14,13 @@ function App() {
       <div className='App-wrapper'>
         <Header />
         <Navbar />
-        <div class='App-wrapper-content'>
+        <div className='App-wrapper-content'>
           <Routes>
             <Route path='/profile' element={<Profile />} />
             <Route path='/dialogs' element={<Dialogs />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/music' element={<Music />} />
+            <Route path='/settings' element={<Settings />} />
           </Routes>
         </div>
       </div>
--- social/src/components/Navbar/Navbar.jsx
+++ social/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,30 @@
 // import React from "react";
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.module.css';
 
+// const setActive = navData => navData.isActive ? classes.active : classes.item; (другое написание для примера)
+const setActive = ({isActive}) => isActive ? classes.active : classes.item;
+
 const Navbar = () => {
-    return <nav className={classes.nav}>
-        <div className={classes.item}>
-            <a href='/profile'>Profile</a>
-        </div>
-        <div className={classes.item}>
-            <a href='/dialogs'>Messages</a>
-        </div>
-        <div className={classes.item}>
-            <a>News</a>
-        </div>
-        <div className={classes.item}>
-            <a>Music</a>
-        </div>
-        <div className={classes.item}>
-            <a>Settings</a>
-        </div>
-    </nav>
+    return (
+        <nav className={classes.nav}>
+            <div>
+                <NavLink to='/profile' className = {setActive}>Profile</NavLink>
+            </div>
+            <div>
+                <NavLink to='/dialogs' className = {setActive}>Messages</NavLink>
+            </div>
+            <div>
+                <NavLink to='/news' className = {setActive}>News</NavLink>
+            </div>
+            <div>
+                <NavLink to='/music' className = {setActive}>Music</NavLink>
+            </div>
+            <div>
+                <NavLink to='/settings' className = {setActive}>Settings</NavLink>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
